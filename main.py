f9a9fcf3a8f86e7b15bd0cab9dfeb45730eb271c
Bryton Smith <Bryton.Smith15@gmail.com>
2026-03-10 21:15:15 -0700

    End of class - unable to pass paramaters to new page

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from searchWTF import AskForTitle
 
 app = Flask(__name__)
-
+app.secret_key = b'(YPT#{@#YAS^RPPF#TA#DGA#adsg' # This should be protected ie don't record it on zoom ;)
+app.config.from_object(__name__)
 @app.route('/')
 def index():
     return render_template ('index.html')
 
 @app.route("/bytitle")
-def aboutme():
-    return render_template ('bytitle.html')
+def bytitle():
+    title_form = AskForTitle()
+    if title_form.validate_on_submit():
+        return "Form Submitted"
+    return render_template ('bytitle.html', form=title_form)
+
+@app.route("/titles", methods=['GET', 'POST'])
+def list_of_titles():
+    searchby = request.form.get('book_title')
+    return f"You searched for {searchby}"
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ searchWTF.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, RadioField, SelectField
+from wtforms.validators import DataRequired, Email, Length, NumberRange, Regexp, URL, InputRequired
+
+class AskForTitle(FlaskForm):
+    book_title = StringField('Enter Book Title to Search for: ')
